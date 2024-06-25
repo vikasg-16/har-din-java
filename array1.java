fb9a1615d64ced647eae217f4629916dbef0403a
vikasg-16 <vikasgupta16699@gmail.com>
2024-06-25 22:06:15 +0530

    t

---
+++ array1.java
@@ -0,0 +1,17 @@
+// This program is to reverse the array  
+
+import java.util.Scanner;
+public class array1 {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        
+        
+    }
+
+    public static int reversearray(int arr[]){
+        int left =0;
+        int right = a;
+        while(left<right)
+    }
+    
+}
