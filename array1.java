0dd6f22c8b41feff8131d335cca291b148c9be3b
vikasg-16 <vikasgupta16699@gmail.com>
2024-06-26 18:56:59 +0530

    t

--- array1.java
+++ array1.java
@@ -1,17 +1,36 @@
-// This program is to reverse the array  
+// This program is to reverse an Integer array  
 
 import java.util.Scanner;
 public class array1 {
     public static void main(String[] args) {
         Scanner s= new Scanner(System.in);
-        
-        
-    }
+        System.out.println("Enter a number of element in an array:");
+        int n=s.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        reversearray(arr);
+        System.out.println("your reversed array:");
+        for (int j : arr) {
+            System.out.println(j+" ");
+        } 
+            
+            
+        }
+    
 
-    public static int reversearray(int arr[]){
+     public static void reversearray(int []arr){
         int left =0;
-        int right = a;
-        while(left<right)
+        int right = arr.length-1;
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+
+        }
     }
     
 }
