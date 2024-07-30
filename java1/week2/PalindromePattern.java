b7659c42ac8a7a9bf1ff3fec66fcd270e1339831
vikasg-16 <vikasgupta16699@gmail.com>
2024-07-30 22:28:56 +0530

    .

---
+++ java1/week2/PalindromePattern.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+//code to print palindrome pattern
+
+public class PalindromePattern{
+public static void main(String [] args){
+Scanner s= new Scanner(System.in);
+System.out.println("Enter the length:");
+int n= s.nextInt();
+for (int i=1;i<=n;i++){
+for (int j=1;j<=n-i;j++){
+System.out.print(" ");
+}
+for (int j=i;j>=1;j--){
+System.out.print(j);
+System.out.print(" ");
+}
+for (int j=2;j<=i;j++){
+System.out.print(j);
+System.out.print(" ");
+}
+System.out.println();
+}
+}
+}
