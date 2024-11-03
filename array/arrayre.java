61dfab4bb3045d3f7e0e823078226a678df2acef
vikasg-16 <vikasgupta16699@gmail.com>
2024-11-03 18:19:43 +0530

    .

--- array/arrayre.java
+++ array/arrayre.java
@@ -1,11 +1,30 @@
 import java.util.*;
 
 /**
- * arrayre
+ * array reverse
  */
 public class arrayre {
 
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        int []arr=new int[5];
+        System.out.println("Enter the elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        int left=0;
+        int right=arr.length-1;
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+        System.out.println("Reversed array:");
+        for(int j:arr){
+            System.out.println(" "+j);
+        }
+
     }
 }
