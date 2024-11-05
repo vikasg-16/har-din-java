d21bd16c9d4a04ca771259153e2b0acb8ab23dc6
vikasg-16 <vikasgupta16699@gmail.com>
2024-11-05 21:06:05 +0530

    .

--- array/remove_duplicate.java
+++ array/remove_duplicate.java
@@ -9,21 +9,33 @@ public class remove_duplicate {
         for(int i=0;i< arr.length;i++){
             arr[i]=sc.nextInt();
         }
-        //Used array list for sorting easily
-        ArrayList<Integer>arrayList=new ArrayList<>();//array list creation
-        //adding element in arraylist 
-        for (Integer inte : arr) {
-            arrayList.add(inte);
-        }
-        Collections.sort(arrayList);
+        Arrays.sort(arr); // sorting array using predefined function
         int []unique_arr=new int[arr.length];
         int j=0;
-        for (int k = 0; k < arrayList.size()-1; k++) {
-            if(arrayList.get(k)!=arrayList.get(k+1)){
-                unique_arr[j++]=arrayList.get(k);
-            }
-        }
-        unique_arr[j++]=arrayList.get(arrayList.size() - 1);
+
+        for (int k = 0; k < arr.length)-1; k++) {
+                 if(arr[k]!=arr[k+1]){
+                     unique_arr[j++]=arr[k];
+                 }
+             }
+             unique_arr[j++]=arr[arr.length-1];
+
+
+        // //Used array list for sorting easily
+        // ArrayList<Integer>arrayList=new ArrayList<>();//array list creation
+        // //adding element in arraylist 
+        // for (Integer inte : arr) {
+        //     arrayList.add(inte);
+        // }
+        // Collections.sort(arrayList);
+        // int []unique_arr=new int[arr.length];
+        // int j=0;
+        // for (int k = 0; k < arrayList.size()-1; k++) {
+        //     if(arrayList.get(k)!=arrayList.get(k+1)){
+        //         unique_arr[j++]=arrayList.get(k);
+        //     }
+        // }
+        // unique_arr[j++]=arrayList.get(arrayList.size() - 1);
 
         for (int i : unique_arr) {
             System.out.println(" "+i);
